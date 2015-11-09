8a55b6b477da353bf9e66b5cd299ab4cced5f6db
major <major@mobilus.co.kr>
2015-11-09 15:03:04 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p /usr/local/log/wilson
+cp vector.def /usr/local/log/wilson
+cp bus/wilson /usr/local/bin
+cp bus/wilson_client /usr/local/bin
+cp run_wilson.sh /usr/local/bin
---
+++ run_wilson.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/local/bin/wilson 14.63.73.40 8088 > /usr/local/log/wilson/server.log &
